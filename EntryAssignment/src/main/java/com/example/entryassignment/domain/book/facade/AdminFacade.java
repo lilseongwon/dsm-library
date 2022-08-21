9423df3397e3659c077b95b9923517405c1e20a7
kim seongwon <seongwon1290@dsm.hs.kr>
2022-08-21 17:17:52 +0900

    ♻️ :: admin 아이디도 yml에 은닉화

--- EntryAssignment/src/main/java/com/example/entryassignment/domain/book/facade/AdminFacade.java
+++ EntryAssignment/src/main/java/com/example/entryassignment/domain/book/facade/AdminFacade.java
@@ -3,6 +3,7 @@ package com.example.entryassignment.domain.book.facade;
 import com.example.entryassignment.domain.book.exception.NoPermissionException;
 import com.example.entryassignment.domain.user.facade.UserFacade;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.Objects;
@@ -13,8 +14,11 @@ public class AdminFacade {
 
     private final UserFacade userFacade;
 
+    @Value("${admin.id}")
+    private String ADMIN;
+
     public void checkPermission() {
-        if (!(Objects.equals(userFacade.getCurrentUser().getAccountId(), System.getenv("ADMIN"))))
+        if (!(Objects.equals(userFacade.getCurrentUser().getAccountId(), ADMIN)))
             throw NoPermissionException.EXCEPTION;
     }
 }
